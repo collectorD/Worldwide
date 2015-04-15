074843695d832ff87f7c542ab7b0ecf558fd02fa
collectorD <collectorD@yandex.com>
2015-04-15 15:47:12 +0200

    Create collection.js

---
+++ collection.js
@@ -0,0 +1,8 @@
+[
+{ "moduleId" : "mapman100/Here"},
+{ "moduleId" : "maps4all/mapquest"},
+{ "moduleId" : "wazemap/waze"},
+{ "moduleId" : "Fritz911/osm"},
+{ "moduleId" : "wikifan/wikimap"},
+{ "moduleId" : "grabowCommuter/Speedometer"}
+]
