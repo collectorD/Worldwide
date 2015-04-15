9ca960898596277b4a5044b7c362b1ff32873d48
collectorD <collectorD@yandex.com>
2015-04-15 17:31:29 +0200

    Update collection.js

--- collection.js
+++ collection.js
@@ -2,7 +2,7 @@
 { "moduleId" : "mapman100/Here"},
 { "moduleId" : "maps4all/mapquest"},
 { "moduleId" : "wazemap/waze"},
-{ "moduleId" : "Fritz911/osm"},
-{ "moduleId" : "wikifan/wikimap"},
-{ "moduleId" : "grabowCommuter/Speedometer"}
+{ "moduleId" : "jamesbond002/osm"},
+{ "moduleId" : "wazemap/wikimap"},
+{ "moduleId" : "grabowCommuter/GoogleSatTraffic"}
 ]
